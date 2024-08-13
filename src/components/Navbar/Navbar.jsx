289dcb64ff9a480d5125a4cc00f4dbd8f9587f0b
Wellick0000 <kashyapshiven2002@gmail.com>
2024-08-13 09:35:51 +0530

    some changes in package json, addred react icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MdOutlineMailOutline, MdOutlineCall } from "react-icons/md";
+import { MdOutlineMailOutline, MdOutlineCall } from 'react-icons/md';
 import ieeelogo from '../../assets/ieeelogo.png';
 import cgclogo from '../../assets/cgclogo.png';
 
